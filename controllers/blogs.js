7f780f4799486cb0cee9a673b01b75ce65af22cd
Tommi Viiala <twviiala@gmail.com>
2018-03-05 12:17:13 +0200

    All tests passed!

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -6,7 +6,7 @@ const User = require('../models/user')
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog
     .find({})
-    .populate('user', { username: 1, name: 1 })
+    .populate('user', {username: 1, name: 1})
 
   response.json(blogs)
 })
@@ -22,13 +22,13 @@ blogRouter.post('/', async (request, response) => {
       return response.status(401).json({ error: 'token missing or invalid' })
     }
 
-    if (title === undefined || url === undefined) {
-      return response.status(400).json({ error: 'url or title missing' })
+    if (title === undefined ||  url === undefined) {
+      return response.status(400).json({ error: 'url or title missing'})
     }
 
     const user = await User.findById(decodedToken.id)
 
-    const blog = new Blog({ title, author, url, likes: (likes || 0), user: user._id })
+    const blog = new Blog({ title, author, url, likes: (likes || 0), user: user._id } )
 
     const result = await blog.save()
 
@@ -47,29 +47,26 @@ blogRouter.post('/', async (request, response) => {
 })
 
 blogRouter.delete('/:id', async (request, response) => {
-  console.log("täälläkö")
+  const blog = await Blog.findById(request.params.id)
 
   try {
-    const blog = await Blog.findById(request.params.id)
     const token = request.token
     const decodedToken = jwt.verify(token, process.env.SECRET)
-    console.log("onko token")
+
     if (!token || !decodedToken.id) {
       return response.status(401).json({ error: 'token missing or invalid' })
     }
-
+    
     console.log(blog.user, decodedToken.id)
 
-    if (decodedToken.id.toString() === blog.user.toString() || blog.user === null) {
-      await blog.remove()
-    } else {
+    if (decodedToken.id.toString() !== blog.user.toString()) {
       return response.status(400).json({ error: 'only creator can delete a blog' })
     }
 
-    // if (blog) {
-    //   await blog.remove()
-    // }
-
+    if (blog) {
+      await blog.remove()
+    }
+    
     response.status(204).end()
   } catch (exception) {
     if (exception.name === 'JsonWebTokenError') {
@@ -83,8 +80,8 @@ blogRouter.delete('/:id', async (request, response) => {
 
 blogRouter.put('/:id', async (request, response) => {
   const { title, author, url, likes } = request.body
-  const blog = await Blog.findByIdAndUpdate(request.params.id, { title, author, url, likes }, { new: true })
-
+  const blog = await Blog.findByIdAndUpdate(request.params.id, { title, author, url, likes } , {new: true})
+  
   response.send(blog)
 })
 
--- controllers/login.js
+++ controllers/login.js
@@ -4,30 +4,25 @@ const loginRouter = require('express').Router()
 const User = require('../models/user')
 
 loginRouter.post('/', async (request, response) => {
-  try {
-    const body = request.body
+  const body = request.body
 
-    const user = await User.findOne({ username: body.username })
-    const passwordCorrect = user === null ?
-      false :
-      await bcrypt.compare(body.password, user.passwordHash)
+  const user = await User.findOne({ username: body.username })
+  const passwordCorrect = user === null ?
+    false :
+    await bcrypt.compare(body.password, user.passwordHash)
 
-    if (!(user && passwordCorrect)) {
-      return response.status(401).send({ error: 'invalid username or password' })
-    }
-
-    const userForToken = {
-      username: user.username,
-      id: user._id
-    }
-
-    const token = jwt.sign(userForToken, process.env.SECRET)
-
-    response.status(200).send({ token, username: user.username, name: user.name })
-  } catch (exception) {
-    console.log(exception)
-    response.status(500)
+  if (!(user && passwordCorrect)) {
+    return response.status(401).send({ error: 'invalid username or password' })
   }
+
+  const userForToken = {
+    username: user.username,
+    id: user._id
+  }
+
+  const token = jwt.sign(userForToken, process.env.SECRET)
+
+  response.status(200).send({ token, username: user.username, name: user.name })
 })
 
 module.exports = loginRouter
