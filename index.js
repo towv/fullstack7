c04d80cc168ebaaa008340f612cb20c88666ff60
Tommi Viiala <twviiala@gmail.com>
2018-03-06 11:05:27 +0200

    Server ongelmana?

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require('http')
+// const http = require('http')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
@@ -31,16 +31,20 @@ app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogRouter)
 
-const server = http.createServer(app)
+// const server = http.createServer(app)
 
-server.listen(config.port, () => {
-  console.log(`Server running on port ${config.port}`)
-})
+// server.listen(config.port, () => {
+//   console.log(`Server running on port ${config.port}`)
+// })
 
-server.on('close', () => {
-  mongoose.connection.close()
-})
+// server.on('close', () => {
+//   mongoose.connection.close()
+// })
+
+// module.exports = {
+//   app, server
+// }
 
 module.exports = {
-  app, server
+  app
 }
