e89fee601a244861c0a136293a839ae891e6ed51
Tommi Viiala <twviiala@gmail.com>
2018-03-06 15:22:55 +0200

    unhandled promise fixed

--- controllers/login.js
+++ controllers/login.js
@@ -5,25 +5,32 @@ const User = require('../models/user')
 
 loginRouter.post('/', async (request, response) => {
   console.log("täällä")
-  const body = request.body
+  try {
 
-  const user = await User.findOne({ username: body.username })
-  const passwordCorrect = user === null ?
-    false :
-    await bcrypt.compare(body.password, user.passwordHash)
 
-  if (!(user && passwordCorrect)) {
-    return response.status(401).send({ error: 'invalid username or password' })
+    const body = request.body
+
+    const user = await User.findOne({ username: body.username })
+    const passwordCorrect = user === null ?
+      false :
+      await bcrypt.compare(body.password, user.passwordHash)
+
+    if (!(user && passwordCorrect)) {
+      return response.status(401).send({ error: 'invalid username or password' })
+    }
+
+    const userForToken = {
+      username: user.username,
+      id: user._id
+    }
+
+    const token = jwt.sign(userForToken, process.env.SECRET)
+
+    response.status(200).send({ token, username: user.username, name: user.name })
+  } catch (exception) {
+    console.log(exception)
+    response.status(500).json({ error: 'something went wrong...' })
   }
-
-  const userForToken = {
-    username: user.username,
-    id: user._id
-  }
-
-  const token = jwt.sign(userForToken, process.env.SECRET)
-
-  response.status(200).send({ token, username: user.username, name: user.name })
 })
 
 module.exports = loginRouter
